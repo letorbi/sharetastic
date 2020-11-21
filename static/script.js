e5683ea462fca452d9efdf1a7002de8686131c8d
Torben <git@letorbi.com>
2020-11-21 16:31:53 +0100

    Add arrayBuffer() polyfill for Safari

--- static/script.js
+++ static/script.js
@@ -1,5 +1,23 @@
 var uploadFiles = new Array();
 
+// Polyfills
+
+// https://gist.github.com/hanayashiki/8dac237671343e7f0b15de617b0051bd
+(function () {
+  File.prototype.arrayBuffer = File.prototype.arrayBuffer || myArrayBuffer;
+  Blob.prototype.arrayBuffer = Blob.prototype.arrayBuffer || myArrayBuffer;
+
+  function myArrayBuffer() {
+    return new Promise((resolve) => {
+      let fr = new FileReader();
+      fr.onload = () => {
+        resolve(fr.result);
+      };
+      fr.readAsArrayBuffer(this);
+    })
+  }
+})();
+
 // DOM
 
 function saveAs(object, filename) {
